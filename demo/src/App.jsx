c6beac343585a90f5a6a91ef75235a63e4fe80dd
Leonardo Gonzalez <gleaonardo383@gmail.com>
2024-08-18 21:24:36 -0300

    add demo project

---
+++ demo/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState([])
+
+  const handleLeftClick=()=>{
+    setAll(allClicks.concat('L'))
+    const updateLeft = left + 1
+    setLeft(updateLeft)
+    setTotal(updateLeft + right)
+  }
+
+  const handleRightClick=()=>{
+    setAll(allClicks.concat('R'))
+    const updateRigth = right + 1
+    setRight(updateRigth)
+    setTotal(updateRigth + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text={'left'}/>
+      <Button handleClick={handleRightClick} text={'right'}/>
+      {right}
+      <History allClicks={allClicks}/>
+    </div>
+  );
+};
+
+const History = (props) => {
+  if(props.allClicks.length === 0){
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+export default App;
